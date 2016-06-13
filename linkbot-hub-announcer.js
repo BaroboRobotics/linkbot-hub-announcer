a99529b9ce4636ec08f43a186d65d8f655f03048
Harris Hancock <harris@barobo.com>
2016-06-13 11:06:01 -0700

    Announce in a loop to make more robust to errors

--- linkbot-hub-announcer.js
+++ linkbot-hub-announcer.js
@@ -18,38 +18,52 @@ var ipAddresses = function () {
         });
     });
     return ipAddresses;
-}();
+};
 
+var switchboardUri = 'https://switchboard.linkbotlabs.com/';
+var switchboardOpts = {
+    endpoints: ['/'],  // Prevent signaller's error event from triggering extra times.
+    autoconnect: true,
+    reconnect: false
+};
 var messenger = require('rtc-switchboard-messenger');
-var signaller = require('rtc-signaller')(messenger('http://switchboard.linkbotlabs.com:4000/'));
+var signaller = require('rtc-signaller')(messenger(switchboardUri, switchboardOpts));
+
+var tenMinutesInMs = 600000;
+var announcementLoop = setInterval(function () {
+    var profile = {
+        room: os.hostname(),
+        type: 'linkbot-hub',
+        ipAddresses: ipAddresses()
+    };
+    signaller.announce(profile);
+}, tenMinutesInMs);
+// Send an announcement every ten minutes to try to keep the connection alive.
+
+var now = function () {
+    return new Date().toISOString();
+}
 
 signaller.on('error', function(err) {
-    console.log('error: ', err);
+    console.log(now(), 'error: ', err);
+    clearInterval(announcementLoop);
+    // If there's an error, quit and let systemd restart us.
 }).on('connected', function() {
-    console.log('connected');
+    console.log(now(), 'connected');
 }).on('disconnected', function() {
-    console.log('disconnected');
+    console.log(now(), 'disconnected');
 }).on('local:announce', function(peer) {
-    console.log('local:announce: ', peer);
+    console.log(now(), 'local:announce: ', peer);
 }).on('peer:filter', function(id, peer) {
-    console.log('peer:filter: ', id, peer);
+    console.log(now(), 'peer:filter: ', id, peer);
 }).on('peer:connected', function(id) {
-    console.log('peer:connected: ', id);
+    console.log(now(), 'peer:connected: ', id);
 }).on('peer:announce', function(peer) {
-    console.log('peer:announce: ', peer);
+    console.log(now(), 'peer:announce: ', peer);
 }).on('peer:update', function(peer) {
-    console.log('peer:update: ', peer);
+    console.log(now(), 'peer:update: ', peer);
 });
 // To add support for a new message type, send a message using this:
 //   signaller.to(peer.id).send('/yourIdentifier', text);  // omit .to(peer.id) to broadcast?
 // And receive a message using this:
 //   signaller.on('message:yourIdentifier', function (text) { ... });
-
-var profile = {
-    room: os.hostname(),
-    type: 'linkbot-hub',
-    ipAddresses: ipAddresses
-};
-
-signaller.announce(profile);
-signaller.connect();
